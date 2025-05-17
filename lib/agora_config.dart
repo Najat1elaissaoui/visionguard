605ef5a38a2cdce6a57f0830677841e57b150d81
Khouulouuda <elazizikhouloud@gmail.com>
2025-05-17 12:59:04 +0100

    Last updates about phone calls

--- lib/agora_config.dart
+++ lib/agora_config.dart
@@ -1,5 +1,5 @@
 class AgoraConfig {
   static const String appId='8a51597d8d1d4efa872e02ade1a83d1d';
-  static const String token='007eJxTYIhxry4K4i08enr65rbgvNXyh64bh5UpHJ5m+ELBwMf4SrICg0WiqaGppXmKRYphiklqWqKFuVGqgVFiSqphooUxUOynnHJGQyAjQ1WrJwsjAwSC+BwMyRmJeXmpOaYMDABwrB+J';
-  static const String channelName='channel5';
+   static const String token='007eJxTYNDSqQlV3ih05l2Xzfr4COFfrw7kmZe5h8Q9CmBLD94WbaTAYJFoamhqaZ5ikWKYYpKalmhhbpRqYJSYkmqYaGEMFJtbrJHREMjIsJ/zDjMjAwSC+BwMyRmJeXmpOZYMDABJnh9e';
+  static const String channelName='channel9';
 }
